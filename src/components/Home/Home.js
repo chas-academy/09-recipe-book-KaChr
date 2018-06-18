b217e0ffd08dae79341b79132755fc2cb504851c
Karin Christensen <karin.christensen@chasacademy.se>
2018-06-18 10:56:21 +0200

    Add BrowserRouter

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>Let's do this!</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from './Header/Header.js';
+export { default as Home } from './Home/Home.js';
+export { default as PageNotFound } from './PageNotFound/PageNotFound.js';
